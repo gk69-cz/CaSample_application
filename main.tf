939edee314c481c88d4e76fdb4eb2cb027380001
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2024-11-11 21:52:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ variable "aws_secret_key" {
 
 provider "aws" {
   region     = "eu-west-1"
-  access_key = var.aws_access_key != "" ? var.aws_access_key : (env("AWS_ACCESS_KEY_ID") != "" ? env("AWS_ACCESS_KEY_ID") : null)
-  secret_key = var.aws_secret_key != "" ? var.aws_secret_key : (env("AWS_SECRET_ACCESS_KEY") != "" ? env("AWS_SECRET_ACCESS_KEY") : null)
+  access_key = var.aws_access_key != "" ? var.aws_access_key : (("AWS_ACCESS_KEY_ID") != "" ? ("AWS_ACCESS_KEY_ID") : null)
+  secret_key = var.aws_secret_key != "" ? var.aws_secret_key : (("AWS_SECRET_ACCESS_KEY") != "" ? ("AWS_SECRET_ACCESS_KEY") : null)
 }
 
 
