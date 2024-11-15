d2338f5a1c6c776d90b58a8dd35c1868ab2b6cf1
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2024-11-15 06:01:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,136 +2,54 @@ provider "azurerm" {
   features {}
 }
 
-variable "client_id" {
-  type        = string
-  description = "Azure client ID"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "Azure client secret"
-}
-
-variable "tenant_id" {
-  type        = string
-  description = "Azure tenant ID"
-}
-
-variable "subscription_id" {
-  type        = string
-  description = "Azure subscription ID"
-}
-
+# 1. Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "app_resource_group"
-  location = "West Europe"
+  name     = "myResourceGroup"
+  location = "East US"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "app_security_group"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "AppPort"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8080"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "app_nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "app_public_ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "app_vm"
+# 2. Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "myacrname"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
+  sku                  = "Basic"
+  admin_enabled        = true
+}
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+# 3. App Service Plan
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "myAppServicePlan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.ssh_key.public_key_openssh
+  sku {
+    tier = "Basic"
+    size = "B1"
   }
 }
 
-resource "local_file" "dynamic_inventory" {
-  filename = "dynamic_inventory.ini"
-  content = <<-EOF
-    [azure_instances]
-    ${azurerm_linux_virtual_machine.vm.public_ip_address} ansible_user=azureuser ansible_ssh_private_key_file=${path.module}/private_key.pem
-  EOF
+# 4. Web App for Containers
+resource "azurerm_web_app" "web_app" {
+  name                = "my-web-app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/nodejs-app:${var.docker_image_tag}"
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"         = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME"    = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"    = azurerm_container_registry.acr.admin_password
+  }
 }
 
-output "public_ip" {
-  value = azurerm_linux_virtual_machine.vm.public_ip_address
+# 5. Output Web App URL
+output "web_app_url" {
+  value = azurerm_web_app.web_app.default_site_hostname
 }
