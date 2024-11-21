e60497e4f766853f796024323fc10028b680bb74
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2024-11-21 23:44:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.use('/api', helloRouter);  // Use the router here with a path prefix
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log('helloo');
 });
